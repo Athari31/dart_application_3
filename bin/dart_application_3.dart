8f1e96e1bd88a014941c293a0e12b78147def985
Athari31 <138482333+Athari31@users.noreply.github.com>
2023-08-21 22:54:19 +0400

    Initial commit

---
+++ bin/dart_application_3.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main(){
+print(475);
+print("Dart course");
+print(true);
+print(false);
+print(33);
+
+// variable ---------------------------------------------------------
+
+}
